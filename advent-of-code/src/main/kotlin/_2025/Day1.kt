4a49e751a8b37b0a09ffb8ca0d0817ff825c4e6f
Tristan Waddington <tristan.waddington@gmail.com>
2025-12-01 22:30:58 -0800

    Finished day 1 part 2.

--- advent-of-code/src/main/kotlin/_2025/Day1.kt
+++ advent-of-code/src/main/kotlin/_2025/Day1.kt
@@ -19,24 +19,34 @@ fun solve(lines: List<String>): Int {
 
     for (line in lines) {
         if (line.isEmpty()) continue
+        val start = position
         val sign = when {
             line.startsWith("L") -> -1
             line.startsWith("R") -> 1
             else -> throw RuntimeException("Invalid line: $line")
         }
-        val rotation = line.substring(1).toInt() % 100
-        position += rotation * sign
+        val rotation = line.substring(1).toInt()
+        position += (rotation % 100) * sign
+
+        val spins = rotation / 100 // Full spins
+        result += spins
 
         if (position < 0) {
             position += 100
+            if (position != 0 && start != 0) {
+                result += 1
+            }
         }
         if (position > 99) {
             position -= 100
+            if (position != 0 && start != 0) {
+                result += 1
+            }
         }
         if (position == 0) {
             result += 1
         }
-//        println("rotation=$rotation, position=$position, result=$result")
+        println("$start,$line, position=$position, result=$result")
     }
     return result
 }
